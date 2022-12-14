e73580232de7c44a29b646e403eea84f16830892
Edwin Hati <edwinhati@outlook.com>
2022-12-14 09:56:37 +0800

    Format Document

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
 const routes = require("./src/router");
-app.use("/",routes);
-app.listen(4000);
+const PORT = 4000;
+app.use("/", routes);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
