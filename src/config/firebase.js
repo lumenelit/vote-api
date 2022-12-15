e52405bb4db8418b27a508691a2a54764fa3a9ee
Edwin Hati <edwinhati@outlook.com>
2022-12-15 18:06:37 +0800

    add firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKoDDZIVxPLR2Rzc6IbGzEzyJ85BCqSJk",
+  authDomain: "uk-voteapp.firebaseapp.com",
+  projectId: "uk-voteapp",
+  storageBucket: "uk-voteapp.appspot.com",
+  messagingSenderId: "259241677135",
+  appId: "1:259241677135:web:8513d238712c2ff989b53a",
+  measurementId: "G-CZT9LWFW6F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+module.exports = app;
