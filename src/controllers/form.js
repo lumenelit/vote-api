89d19d8054a61a9ecaa1304bfc18d5d1b54e7f32
Edwin Hati <edwinhati@outlook.com>
2022-12-16 08:55:08 +0800

    add create function

--- src/controllers/form.js
+++ src/controllers/form.js
@@ -1,15 +1,24 @@
-export function create(req, res) {
-    //
+import { doc, setDoc, getFirestore } from "firebase/firestore";
+import firebase from "../config/firebase.js";
+const db = getFirestore(firebase);
+
+export async function create(req, res) {
+  const { information, fields } = req.body;
+  await setDoc(doc(db, "forms", req.params.token), {
+    information,
+    fields,
+    VotedList: [],
+  }).then(res.status(201).end());
 }
 export function list(req, res) {
-    //
+  //
 }
 export function get(req, res) {
-    //
+  //
 }
 export function submit(req, res) {
-    //
+  //
 }
 export function report(req, res) {
-    //
+  //
 }
