2d9fad908d80af6a0489b0d2b726913c624385b2
tumewubenedict <tbenedictjonathan@gmail.com>
2022-12-20 21:17:34 +0800

    function report

--- src/controllers/form.js
+++ src/controllers/form.js
@@ -39,6 +39,15 @@ export async function get(req, res) {
 export function submit(req, res) {
   //
 }
-export function report(req, res) {
-  //
+export async function report(req, res) {
+  const docRef = doc(db,"forms",req.params.token);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    const data = docSnap.data()
+    const title = data.fields[0].options[0].title;
+    const votes = data.fields[0].options[0].votes;
+    res.json({title,votes});
+  } else {
+    res.send("No such document!")
+  }
 }
