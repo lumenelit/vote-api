93e86f33301d425080e40a82e4b7d1353b214785
Edwin Hati <edwinhati@outlook.com>
2022-12-27 10:35:37 +0800

    Add CORS bypass to index.js
    The CORS policy, or the Cross-Origin Resource Sharing policy, prevents accessing web resources from sources other than the server the website is running on for security purposes.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from "express";
 const app = express();
 import routes from "./src/router/index.js";
 const PORT = 4000;
+
+// cors bypass
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+})
+
 app.use(express.json());
 app.use("/", routes);
 app.listen(PORT, () => {
